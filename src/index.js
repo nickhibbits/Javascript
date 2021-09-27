caabe5b4fb5df1f5f7fc88c8cf32f6dacf4e7688
Nicholas Hibbits <nicholashibbits@gmail.com>
2021-09-26 17:43:40 -0700

    update scripts, click event

--- src/index.js
+++ src/index.js
@@ -3,6 +3,5 @@ import {onClick} from './client/app.js';
 import './client/app.scss'
 
 let button = document.querySelector("#btn");
-console.log("button", button)
 
 button.addEventListener("click", onClick)
