3832107a6944802ba8f3d6824bd48ebc192ca0a5
Nicholas Hibbits <nicholashibbits@gmail.com>
2021-10-05 19:57:22 -0700

    Elaborate loops to create tiles w/ facts

--- src/client/app.js
+++ src/client/app.js
@@ -1,3 +1,4 @@
+import { DiagnosticCategory } from "typescript";
 import { Dinos } from "../../dino.json"
 
 let dino = Dinos;
@@ -22,7 +23,7 @@ function Dino(species, weight, height, diet, where, when, fact) {
 }
 
 // Create Dino Objects
-// **Needs DRY code** -- use loop
+// **Needs DRY code** -- use loop to create an array "dinoSet", that you can call in the loop to create dino squares
 let triceratops = new Dino(dino[0].species, dino[0].weight, dino[0].height, dino[0].diet, dino[0].where, dino[0].when, dino[0].fact)
 let tRex = new Dino(dino[1].species, dino[1].weight, dino[1].height, dino[1].diet, dino[1].where, dino[1].when, dino[1].fact)
 let anklyosaurus = new Dino(dino[2].species, dino[2].weight, dino[2].height, dino[2].diet, dino[2].where, dino[2].when, dino[2].fact)
@@ -43,53 +44,89 @@ Dino.prototype.compareDiet = () => {}
 
 // Create Human Object -- with constructor or with object literal?
 function Human(name, feet, inches, weight, diet) {
-    this.name = name,
-    this.heightFeet = feet,
-    this.heightInches = inches,
-    this.weight = weight,
-    this.diet = diet
+  this.name = name,
+  this.heightFeet = feet,
+  this.heightInches = inches,
+  this.weight = weight,
+  this.diet = diet
 }
 
 // Add tiles to DOM... with function?
 function addTiles() {
-    console.log("add tiles")
+  console.log("add tiles")
 }
 
 function onClick (event) {
-    event.preventDefault();
+  event.preventDefault();
 
-    // Use IIFE (??) to get human data from form
-    let user = new Human(nameField.value, feetField.value, inchesField.value, weightField.value, dietField.value);
+  // Use IIFE (??) to get human data from form
+  let user = new Human(nameField.value, feetField.value, inchesField.value, weightField.value, dietField.value);
 
-    // dynamically create dino tiles
-    // **Needs DRY code**
-    // use a loop index to set the id of each square "dino-one, dino-two, etc"
+  // dynamically create dino tiles
+  // **Needs DRY code**
+  // use a loop index to set the id of each square "dino-one, dino-two, etc"
+  for (let i = 0; i < dinoSet.length; i++ ) {
     let dinoBlock = document.createElement("section");
     dinoBlock.classList.add("grid-item");
-    grid.appendChild(dinoBlock);
+    dinoBlock.setAttribute("id", `dino-${i}`)
 
+    //grab dino facts, create fields within block, populate fields with facts
+    for (let x = 0; x < Object.keys(dinoSet[x]).length; x++) {
+      let field = document.createElement("div");
+      field.setAttribute("id", `dino-fact-${x}`)
 
-    // dynamically create human tile
-    // set user data to center tile -- Object.values(user)
-    let humanBlock = document.createElement("section");
-    for (let i = 0; i < user.length(); i++) {
-        let blockField = document.createElement("div")
-        blockField.classList.add(`human${i}`);
+      dinoFacts = Object.values(dinoSet[x]);
+      let dinoFact = dinoFacts[x];
 
-        let fact = user[i]
-        
-        humanBlock.appendChild(blockField);
+      field.innerHTML = dinoFact;
+
+      dinoBlock.appendChild(field)
     }
 
-    humanBlock.classList.add("grid-item");
-    grid.appendChild(humanBlock);
+    grid.appendChild(dinoBlock);
+  }
 
-    //dynamically create pigeon tile
 
-    console.log("user", user);
+  // dynamically create human tile
+  let humanBlock = document.createElement("section");
+  humanBlock.classList.add("grid-item");
+  humanBlock.setAttribute("id", "human");
 
-    form.style = "display: none";
-    grid.style = "display: grid";
+  for (let i = 0; i < Object.keys(user).length; i++) {
+    let blockField = document.createElement("div");
+    blockField.setAttribute("id", `human-fact-${i}`);
+
+    let userFacts = Object.values(user)
+    let userFact = userFacts[i]
+
+    blockField.innerHTML = userFact
+        
+    humanBlock.appendChild(blockField);
+  }
+
+  grid.appendChild(humanBlock);
+
+  //dynamically create pigeon tile
+  let pigeonBlock = document.createElement("section");
+  pigeonBlock.classList.add("grid-item");
+  pigeonBlock.setAttribute("id", "pigeon");
+
+  for (let i = 0; i < Object.keys(user).length; i++) {
+    let blockField = document.createElement("div");
+    blockField.setAttribute("id", `pigeon-fact-${i}`);
+
+    let userFacts = Object.values(user)
+    let userFact = userFacts[i]
+
+    blockField.innerHTML = userFact
+        
+    pigeonBlock.appendChild(blockField);
+  }
+
+  grid.appendChild(pigeonBlock);
+
+  form.style = "display: none";
+  grid.style = "display: grid";
 }
 
 export { onClick }
